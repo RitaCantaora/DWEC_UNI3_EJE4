d22211055684c31d345cc8c1d103c24cef7a12ba
mdegs02 <mdegs02@educastur.es>
2023-11-09 18:06:42 +0100

    propuesto4

--- js/propuesto4.js
+++ js/propuesto4.js
@@ -1,1 +1,21 @@
+function pulsaciones(event){
+    let nombreAnimal = event.target.getAttribute('class');
+    let textoUltimo = document.getElementById('ultimo');
+    textoUltimo.innerHTML = nombreAnimal;
+    textoUltimo.style.backgroundColor = 'yellow';
+}
 
+function inicializar() {
+    let imagenes = document.getElementsByClassName('perro');
+    for (let i = 0; i <imagenes.length; i++) {
+        imagenes[i].addEventListener('click', pulsaciones);
+    }
+
+    let imagen = document.getElementsByClassName('gato');
+    for(let i =0 ; i<imagen.length ; i++){
+
+        imagen[i].addEventListener('click',pulsaciones);
+    }
+
+}
+window.addEventListener('load', inicializar);
